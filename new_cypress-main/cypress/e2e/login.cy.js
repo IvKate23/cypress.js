d82ec932eebbcd79c28c0238c61623c7e9d03159
IvKate23 <ivkate.foto@gmail.com>
2025-04-16 16:38:06 +0300

    Add files via upload

---
+++ new_cypress-main/cypress/e2e/login.cy.js
@@ -0,0 +1,70 @@
+describe('Проверка формы логина и пароля', function () {
+
+     it('Верный логин и верный пароль', function () {
+         cy.visit('https://login.qa.studio/');
+         cy.get('#mail').type('german@dolnikov.ru');
+         cy.get('#pass').type('iLoveqastudio1');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+      })
+
+
+      it('Восстановление пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+
+     it('Верный логин и НЕверный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveq');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+
+     it('НЕверный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germannn@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+
+     it('Логин без @ и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+
+     it('Заглавные буквы вместо строчных в логине и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+ })
+ 
+ 
+ 
---
+++ new_cypress-main/cypress/e2e/pokemons.cy.js
@@ -0,0 +1,27 @@
+describe('Покупка нового аватара для своего тернера', function () {
+
+    it('e2e проверка покупки нового аватара для своего тренера', function () {
+        cy.visit('https://pokemonbattle.ru/');
+        cy.get('#k_email').type('USER_LOGIN');
+        cy.get('#k_password').type('USER_PASSWORD');
+        cy.get('.MuiButton-root').click();
+        cy.wait(2000);
+
+        cy.get('.header_card_trainer').click();
+        cy.get('.k_mobile > :nth-child(5)').click();
+        cy.get('.available > .shop__button').first().click(); 
+
+        cy.get('.payment_form_card_form > :nth-child(2) > .style_1_base_input').type('4620869113632996');
+        cy.get(':nth-child(1) > .style_1_base_input').type('1226');
+        cy.get('.payment_form_card_form_inputs > :nth-child(2) > .style_1_base_input').type('125');  
+        cy.get('.payment_form_card_form_input_last > .style_1_base_input').type('NAME');
+        cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();
+
+        cy.get('.style_1_base_input').type('56456'); 
+        cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();
+        
+        cy.contains('Покупка прошла успешно').should('be.visible');
+
+     })
+
+})
